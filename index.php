0b4b57f51914f36d9f59599ef1ebec4a8a62fb1d
kinocon <kino@consultant>
2011-01-05 16:28:12 +0000

    yii project

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
-<!doctype html>
-<html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title></title>
-  </head>
-  <body>
-    <?php
-    // put your code here
-    ?>
-  </body>
-</html>
+<?php
+
+// change the following paths if necessary
+$yii=dirname(__FILE__).'/yii/framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
+
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once($yii);
+Yii::createWebApplication($config)->run();
